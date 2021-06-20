04c058352a397b284af1bbe230b4525da2bbbff6
Ryan Dawkes <rd@moose.co>
2021-06-20 12:22:41 +0100

    add endpoint for char types

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CharactersController;
+use App\Http\Controllers\CharacterTypesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('characters', CharactersController::class);
+Route::apiResource('character-types', CharacterTypesController::class);
