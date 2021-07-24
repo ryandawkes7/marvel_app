8ce77bd5ff99a6f9d853047f871b5a42483358f0
Ryan Dawkes <rd@moose.co>
2021-07-24 23:08:51 +0100

    refactor skill grid into component

--- resources/js/Pages/Characters/Show.vue
+++ resources/js/Pages/Characters/Show.vue
@@ -80,51 +80,7 @@
             </section>
 
             <!-- Skill Grid -->
-            <section aria-labelledby="skill-grid">
-              <div class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-1 sm:gap-px">
-                <div class="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none sm:rounded-tr-lg sm:rounded-bl-lg rounded-bl-lg rounded-br-lg sm:rounded-bl-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-500">
-                  <div>
-                    <!-- Heading -->
-                    <div class="my-4 flex flex-col gap-1">
-
-                      <!-- Title -->
-                      <div class="flex justify-between">
-                        <h3 class="text-lg font-medium">
-                            Skill Grid
-                        </h3>
-                        <ExternalLinkIcon class="h-5 w-5" />
-                      </div>
-
-                      <!-- Subheading -->
-                      <p class="text-sm text-gray-500" v-if="character.alias != ''">
-                        Get an insight into {{character.alias}}'s competence in the field. Click on this box to gain further insight into each type of skill.
-                      </p>
-                    </div>
-
-                    <!-- Grid -->
-                    <div class="flex flex-col">
-                      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                          <div class="overflow-hidden border-b border-gray-200 rounded-lg">
-
-                            <div class="block border border-gray-100">
-                              <div v-for="skill in skills" :key="skill.id" :class="`bg-${skill.colour}-100`" class="grid grid-cols-12 p-3">
-                                <p :class="`text-${skill.colour}-700`" class="col-span-3 text-wrap capitalize">{{skill.name}}</p>
-                                <div class="col-span-9 rounded min-h-5 px-2 my-auto" :class="`bg-${skill.colour}-400`" :style="{width: `${skill.value}%`}">
-                                  <span class="opacity-0">.</span>
-                                </div>
-                              </div>
-                            </div>
-                            
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-            </section>
+            <SkillGrid :alias="character.alias" :skills="character.skills"></SkillGrid>
           </div>
 
           <!-- Right column -->
@@ -564,6 +520,7 @@
   import MultiSelect from '../Components/MultiSelect.vue';
   import DeleteCharacterModal from '../Components/DeleteCharacterModal.vue';
   import SuccessBanner from '../Components/SuccessBanner.vue';
+  import SkillGrid from './SkillGrid.vue';
   import axios from 'axios';
 
   export default {
@@ -650,6 +607,7 @@
         MenuItems,
         MultiSelect,
         PencilIcon,
+        SkillGrid,
         SuccessBanner,
         TransitionChild,
         TransitionRoot,
@@ -671,28 +629,33 @@
           item.id != section.id ? item.current = false : setSection(item);
         });
 
-        if (activeSection.id == 2) {
-          this.isProfile = false;
-          this.isComics = true;
-          this.isTeams = false;
-          this.isMovies = false;
-        } else if (activeSection.id == 3) {
-          this.isProfile = false;
-          this.isComics = false;
-          this.isTeams = true;
-          this.isMovies = false;
-        } else if (activeSection.id == 4) {
-          this.isProfile = false;
-          this.isComics = false;
-          this.isTeams = false;
-          this.isMovies = true;
-        } else {
-          this.isProfile = true;
-          this.isComics = false;
-          this.isTeams = false;
-          this.isMovies = false;
+        switch(activeSection.id) {
+          case 1:
+            this.isProfile = true;
+            this.isComics = false;
+            this.isTeams = false;
+            this.isMovies = false;
+          case 2: 
+            this.isProfile = false;
+            this.isComics = true;
+            this.isTeams = false;
+            this.isMovies = false;
+            break;
+          case 3:
+            this.isProfile = false;
+            this.isComics = false;
+            this.isTeams = true;
+            this.isMovies = false;
+            break;
+          case 4:
+            this.isProfile = false;
+            this.isComics = false;
+            this.isTeams = false;
+            this.isMovies = true;
+            break;
         }
       },
+
       fetchId() {
         return window.location.href.split('/').pop();
       },
@@ -704,7 +667,7 @@
         // Fetch character data
         axios.get(`/api/characters/${id}`)
           .then(res => {
-            const data = res.data.data[0];
+            const data = res.data.data;
             this.character = data;
             this.character.type_id = data.type_id.toString();
 
