5f4edfd30f40c85bf622d3ca5f2db2de01be19c6
Ryan Dawkes <rd@moose.co>
2021-08-01 15:31:11 +0100

    Add specific movie view and edit modal

---
+++ resources/js/Pages/Movies/EditModal.vue
@@ -0,0 +1,78 @@
+<template>
+
+<div class="fixed z-10 inset-0 overflow-y-auto max-h-screen" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="flex items-end justify-center max-h-screen text-center sm:block mx-4 sm:p-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+        <div class="inline-block max-h-screen h-100 align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-7xl sm:w-full sm:p-6">
+            <div class="bg-white overflow-hidden flex">
+
+            <!-- Sidebar -->
+            <div class="hidden md:flex md:flex-shrink-0">
+                <div class="w-64 flex flex-col">
+                    <div class="border-r border-gray-200 pt-5 pb-4 flex flex-col flex-grow overflow-y-auto">
+
+                        <!-- Title -->
+                        <div class="flex-shrink-0 px-4 flex items-center">
+                            <h2 class="h-8 w-auto font-bold text-xl">Movies</h2>
+                        </div>
+
+                        <!-- Sub-Page Links -->
+                        <div class="flex-grow mt-5 flex flex-col">
+                            <nav class="flex-1 bg-white px-2 space-y-1">
+
+                                <!-- Movie Details -->
+                                <a href="#" class="bg-gray-100 text-gray-900 group rounded-md py-2 px-2 flex items-center text-sm font-medium">
+                                    <svg class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                                    Details
+                                </a>
+
+                                <a href="#" class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group rounded-md py-2 px-2 flex items-center text-sm font-medium">
+                                    <svg class="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+                                    Posters
+                                </a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!--  Body -->
+            <div class="flex-1 max-w-4xl mx-auto w-0 flex flex-col md:px-8 xl:px-0">
+
+                <!-- Open Sidebar Button (Mobile) -->
+                <button class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
+                    <span class="sr-only">Open sidebar</span>
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                </button>
+
+                <!-- Viewport -->
+                <main class="flex-1 relative overflow-y-auto focus:outline-none">
+                    <div class="py-6">
+                        <div class="px-4 sm:px-6 md:px-0">
+                            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                        </div>
+                        <div class="px-4 sm:px-6 md:px-0">
+                            <!-- Replace with your content -->
+                            <div class="py-4">
+                                <div class="h-96 border-4 border-dashed border-gray-200 rounded-lg"></div>
+                            </div>
+                            <!-- /End replace -->
+                        </div>
+                    </div>
+                </main>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ resources/js/Pages/Movies/Show.vue
@@ -0,0 +1,265 @@
+<template>
+    <app-layout>
+        <!-- Header -->
+        <header class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 mb-4 flex">
+            <Menu as="div" class="relative inline-block text-left ml-auto">
+                <div>
+                    <MenuButton class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                        Options
+                        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                    </MenuButton>
+                </div>
+
+                <!-- Menu Items -->
+                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                    <MenuItems class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div class="py-1">
+
+                            <!-- Edit Character -->
+                            <MenuItem>
+                                <button @click="setIsEditModalOpen(true)" class="flex items-center block px-4 py-2 text-sm gap-2 w-full" ref="editButtonRef">
+                                    <PencilIcon class="w-5 h-5 text-gray-500" />
+                                    <p class="text-gray-900">Edit</p>
+                                </button>
+                            </MenuItem>
+
+                            <!-- Delete Character -->
+                            <MenuItem>
+                                <button @click="toggleDeleteModal()" class="flex items-center block px-4 py-2 text-sm gap-2 w-full" ref="deleteButtonRef">
+                                    <TrashIcon class="w-5 h-5 text-gray-500"/>
+                                    <p class="text-gray-900">Delete</p>
+                                </button>
+                            </MenuItem>
+                        </div>
+                    </MenuItems>
+                </transition>
+            </Menu>
+        </header>
+
+        <!-- Body -->
+        <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div class="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
+
+                <!-- Left column -->
+                <div class="grid grid-cols-1 gap-4 lg:col-span-2">
+
+                    <!-- Description panel -->
+                    <section aria-labelledby="profile-overview-title">
+                        <div class="rounded-lg bg-white overflow-hidden shadow">
+                            <div class="bg-white p-6">
+                                <div class="sm:flex sm:items-center sm:justify-between">
+                                    <div class="sm:flex sm:space-x-5">
+                                        <div class="flex-shrink-0">
+                                            <div class="relative">
+                                                <div class="w-20 h-20 flex-shrink-0 mx-auto bg-white rounded-full overflow-hidden shadow-lg">
+                                                    <img class="object-contain min-w-full min-h-full"
+                                                        :src="movie.image ? movie.image : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ZsrCV912yo&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'"
+                                                    />
+                                                </div>
+                                                <div class="group absolute -top-1 -right-1 rounded-lg overflow-hidden bg-gray-200 cursor-pointer transition-colors flex p-0.5 hover:bg-red-100" @click="isPosterModalOpen = true">
+                                                    <ArrowsExpandIcon class="h-6 w-6 text-white m-auto" stroke="#000000"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="mt-4 sm:mt-0 sm:pt-1 sm:text-left flex flex-col justify-center">
+                                            <p class="text-xl font-bold text-gray-900 sm:text-2xl">{{movie.title}}</p>
+                                            <p class="text-gray-400 font-light sm:text-md">Director: {{movie.director}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="mt-5 flex justify-center sm:mt-0">
+                                        <a href="#" class="flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+                                            View profile
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="border-t border-gray-200 bg-gray-50 grid grid-cols-1 divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
+                                <div  class="px-6 py-5 text-sm font-medium text-center">
+                                    <span class="text-gray-900">Stat Value</span>
+                                    {{ ' ' }}
+                                    <span class="text-gray-600">Stat Label</span>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <!-- Skill Grid -->
+                    <!-- <SkillGrid :alias="character.alias" :skills="character.skills"></SkillGrid> -->
+                </div>
+
+                <!-- Right column -->
+                <div class="grid grid-cols-1 gap-4">
+                
+                    <section aria-labelledby="skill-grid">
+                        <div class="rounded-lg bg-white overflow-hidden shadow">
+
+                            <!-- Contents -->
+                            <div class="p-6">
+
+                                <!-- Title -->
+                                <h2 class="text-base font-medium text-gray-900" id="announcements-title">Section</h2>
+
+                                <!-- Grid -->
+                                <div class="flex flex-col">
+                                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                                <div class="block border border-gray-100">
+                                                </div>
+                            
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Action Button -->
+                                <div class="mt-6">
+                                    <a href="#" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+                                        View all
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <!-- Recent Hires -->
+                    <section aria-labelledby="recent-hires-title">
+                        <div class="rounded-lg bg-white overflow-hidden shadow">
+                            <div class="p-6">
+                                <h2 class="text-base font-medium text-gray-900" id="recent-hires-title">Recent Hires</h2>
+                                <div class="flow-root mt-6">
+                                    <ul class="-my-5 divide-y divide-gray-200">
+                                        <li class="py-4">
+                                            <div class="flex items-center space-x-4">
+                                                <div class="flex-shrink-0">
+                                                    <img class="h-8 w-8 rounded-full"  alt="" />
+                                                </div>
+                                                <div class="flex-1 min-w-0">
+                                                    <p class="text-sm font-medium text-gray-900 truncate">
+                                                        Name
+                                                    </p>
+                                                    <p class="text-sm text-gray-500 truncate">
+                                                        @handle
+                                                    </p>
+                                                </div>
+                                                <div>
+                                                    <a class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50">
+                                                        View
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="mt-6">
+                                <a href="#" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+                                    View all
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Poster Modal -->
+        <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" v-if="isPosterModalOpen">
+            <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" @click="setIsPosterModalOpen(false)"></div>
+
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div class="inline-block align-bottom overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-md sm:max-h-screen sm:w-full">
+                    <img class="max-h-screen "
+                        :src="movie.image ? movie.image : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ZsrCV912yo&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <!-- Edit Modal -->
+        <EditModal v-if="isEditModalOpen"></EditModal>
+
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout';
+import { ref } from 'vue';
+import { Dialog, DialogOverlay, DialogTitle, Menu, MenuButton, MenuItem, MenuItems, TransitionChild, TransitionRoot } from '@headlessui/vue';
+import { BookOpenIcon, ChevronDownIcon, FilmIcon, PencilIcon, TrashIcon, UserCircleIcon, UserGroupIcon } from '@heroicons/vue/solid'
+import { ArrowsExpandIcon } from '@heroicons/vue/solid'
+import EditModal from './EditModal.vue';
+
+export default {
+    components: {
+        AppLayout,
+        ArrowsExpandIcon,
+        BookOpenIcon, 
+        ChevronDownIcon,
+        Dialog,
+        DialogOverlay,
+        DialogTitle,
+        EditModal,
+        FilmIcon,
+        Menu,
+        MenuButton,
+        MenuItem,
+        MenuItems,
+        PencilIcon,
+        TransitionChild,
+        TransitionRoot,
+        TrashIcon, 
+        UserCircleIcon, 
+        UserGroupIcon
+    },
+    created() {
+        this.fetchCharacter();
+    },
+    data() {
+        return {
+            movie: {
+                title: String,
+                director: String,
+                release_date: Date,
+                in_mcu: Boolean,
+                mcu_phase_id: Number,
+                image: String,
+            },
+        }
+    },
+    methods: {
+        fetchId() {
+            return window.location.href.split('/').pop();
+        },
+
+        fetchCharacter() {
+            const id = this.fetchId();
+
+            // Fetch character data
+            axios.get(`/api/movies/${id}`)
+            .then(res => {
+                const data = res.data.data;
+                this.movie = data;
+                console.log(this.movie.sagas);
+            })
+        },
+    },
+    setup() {
+        const isPosterModalOpen = ref(false);
+        const isEditModalOpen = ref(true);
+
+        return {
+            isPosterModalOpen,
+            setIsPosterModalOpen(value) {
+                isPosterModalOpen.value = value;
+            },
+            isEditModalOpen,
+            setIsEditModalOpen(value) {
+                isEditModalOpen.value = value;
+            }
+        }
+    }
+}
+</script>
