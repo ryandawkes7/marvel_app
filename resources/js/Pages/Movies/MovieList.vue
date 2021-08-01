3d23b68f24adf1d0df8dcd5184ef2afc5f58c3f9
Ryan Dawkes <rd@moose.co>
2021-08-01 15:30:55 +0100

    Modify movie list viewport

--- resources/js/Pages/Movies/MovieList.vue
+++ resources/js/Pages/Movies/MovieList.vue
@@ -1,7 +1,7 @@
 
 <template>
     <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <li v-for="movie in this.movies" :key="movie.id" class="group col-span-1 grid grid-cols-4 bg-white rounded-lg overflow-hidden shadow divide-y divide-gray-200 transition-all hover:bg-red-50 hover:shadow-lg cursor-pointer">
+        <li v-for="movie in this.movies" :key="movie.id" @click="showMovie(movie.id)" class="group col-span-1 grid grid-cols-4 box-border bg-white rounded-lg overflow-hidden shadow divide-y divide-gray-200 transition-all hover:bg-red-50 hover:shadow-lg cursor-pointer">
             <div class="col-span-3 flex items-center justify-between p-6 space-x-6">
                 <div class="flex-1 truncate">
                     <div class="flex items-center space-x-3">
@@ -11,7 +11,9 @@
                     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ movie.release_date }}</span>
                 </div>
             </div>
-            <img class="col-span-1 min-h-full bg-gray-300 flex-shrink-0 group-hover:bg-red-500" v-if="movie.image" :src="movie.image" alt="" />
+            <div class="col-span-1 bg-gray-300 flex-shrink-0 transition-all group-hover:bg-red-500">
+                <img class="min-h-full transition-all object-cover group-hover:my-1" v-if="movie.image" :src="movie.image" alt="" />
+            </div>
         </li>
   </ul>
 </template>
@@ -31,12 +33,16 @@ export default {
         setMovies() {
             if (this.movies) return this.movies;
         },
+        // TODO: Allow custom sorting (i.e. user can choose to sort by title, director, release date)
         sortMovies(key) {
             this.movies.sort((a,b) => {
                 if (a[key] > b[key]) return 1;
                 if (b[key] > a[key]) return -1;
                 return 0;
             });
+        },
+        showMovie(id) {
+            window.location.href = `/movies/${id}`;
         }
     },
     props: {
