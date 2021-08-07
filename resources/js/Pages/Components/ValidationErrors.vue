c413e7fb1f2f0fa20b20bb4c63a16013709a42f7
Ryan Dawkes <rd@moose.co>
2021-08-07 18:06:47 +0100

    Support validation errors from Laravel

---
+++ resources/js/Pages/Components/ValidationErrors.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="validationErrors" class="pt-2">
+        <ul class="bg-red-100 px-2 py-1 rounded-md">
+            <li v-for="(value, key) in validationErrors" :key="key">
+                <p class="text-red-500"><span class="font-bold mr-1">-</span> {{value}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        validationErrors() {
+            let errors = Object.values(this.errors);
+            errors = errors.flat();
+            return errors;
+        }
+    },
+    props: ['errors']
+}
+</script>
