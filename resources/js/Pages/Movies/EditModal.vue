aa72b709332ae99ef48c0b831de67cb358f1f849
Ryan Dawkes <rd@moose.co>
2021-08-14 20:10:33 +0100

    Remove debug

--- resources/js/Pages/Movies/EditModal.vue
+++ resources/js/Pages/Movies/EditModal.vue
@@ -126,10 +126,7 @@
                                 <div>
                                     <button type="button" class="-mr-3 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900">
                                         <span class="sr-only">Open sidebar</span>
-                                        <!-- Heroicon name: outline/menu -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                        </svg>
+                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
                                     </button>
                                 </div>
                             </div>
