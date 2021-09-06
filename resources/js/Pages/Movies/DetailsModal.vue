0d41d1e2b2af25e9d4a56e890b2102a21e8f12e9
Ryan Dawkes <rd@moose.co>
2021-09-06 16:37:02 +0100

    Search support on sagas and directors

--- resources/js/Pages/Movies/DetailsModal.vue
+++ resources/js/Pages/Movies/DetailsModal.vue
@@ -27,10 +27,22 @@
                         <div class="lg:col-span-4 sm:col-span-6">
                             <label for="director" class="block text-sm font-medium text-gray-700">Directors</label>
 
-                            <div class="border border-gray-300 shadow-md rounded-md p-2 flex flex-wrap gap-2">
-                                <span v-for="director in directors" :key="director.id" v-on="movieDirectorIds.includes(director.id) ? {click: () => removeSelectedDirector(director)} : {click: () => addSelectedDirector(director)}" class="transition-colors px-2 py-0.5 rounded-full whitespace-nowrap cursor-pointer text-sm" :class="movieDirectorIds.includes(director.id) ? 'bg-green-400 text-green-100 hover:bg-green-500 ' : 'bg-red-400 hover:bg-red-500 text-red-100'">
-                                    {{director.name}}
-                                </span>
+                            <div class="border border-gray-300 shadow-md rounded-md p-2 flex flex-col gap-2">
+                                <!-- Search Bar -->
+                                <div class="flex justify-between items-center w-full relative rounded-md bg-gray-50 border-0">
+                                    <input type="text" v-model="directorSearch" class="w-full transition-colors border-0 bg-transparent rounded-md focus:bg-red-50" placeholder="Search directors..." />
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-2 my-auto h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                </div>
+
+                                <!-- Names -->
+                                <div v-if="directors.length > 0" class="flex flex-wrap gap-2">
+                                    <span v-for="director in directors" :key="director.id" v-on="movieDirectorIds.includes(director.id) ? {click: () => removeSelectedDirector(director)} : {click: () => addSelectedDirector(director)}" class="transition-colors px-2 py-0.5 rounded-full whitespace-nowrap cursor-pointer text-sm" :class="movieDirectorIds.includes(director.id) ? 'bg-green-400 text-green-100 hover:bg-green-500 ' : 'bg-red-400 hover:bg-red-500 text-red-100'">
+                                        <div>{{director.name}}</div>
+                                    </span>
+                                </div>
+                                <div v-else class="flex justify-center py-3 bg-gray-50 text-gray-400 font-bold rounded-md w-full">
+                                    No results found
+                                </div>
                             </div>
                         </div>
 
@@ -68,10 +80,22 @@
                         <div class="col-span-4 sm:col-span-4">
                             <label for="saga" class="block text-sm font-medium text-gray-700">Associated Saga</label>
 
-                            <div class="border border-gray-300 shadow-md rounded-md p-2 flex flex-wrap gap-2">
-                                <span v-for="saga in sagas" :key="saga.id" v-on="movieSagaIds.includes(saga.id) ? {click: () => removeSelectedSaga(saga)} : {click: () => addSelectedSaga(saga)}" class="transition-colors px-2 py-0.5 rounded-full whitespace-nowrap cursor-pointer text-sm" :class="movieSagaIds.includes(saga.id) ? 'bg-green-400 text-green-100 hover:bg-green-500 ' : 'bg-red-400 hover:bg-red-500 text-red-100'">
-                                    {{saga.title}}
-                                </span>
+                            <div class="border border-gray-300 shadow-md rounded-md p-2 flex flex-col gap-2">
+                                <!-- Search Bar -->
+                                <div class="flex justify-between items-center w-full relative rounded-md bg-gray-50 border-0">
+                                    <input type="text" v-model="sagaSearch" class="w-full transition-colors border-0 bg-transparent rounded-md focus:bg-red-50" placeholder="Search sagas..." />
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-2 my-auto h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                </div>
+
+                                <!-- Results -->
+                                <div v-if="sagas.length > 0" class="flex flex-wrap gap-2">
+                                    <span v-for="saga in sagas" :key="saga.id" v-on="movieSagaIds.includes(saga.id) ? {click: () => removeSelectedSaga(saga)} : {click: () => addSelectedSaga(saga)}" class="transition-colors px-2 py-0.5 rounded-full whitespace-nowrap cursor-pointer text-sm" :class="movieSagaIds.includes(saga.id) ? 'bg-green-400 text-green-100 hover:bg-green-500 ' : 'bg-red-400 hover:bg-red-500 text-red-100'">
+                                        <div>{{saga.title}}</div>
+                                    </span>
+                                </div>
+                                <div v-else class="flex justify-center py-3 bg-gray-50 text-gray-400 font-bold rounded-md w-full">
+                                    No results found
+                                </div>
                             </div>
                         </div>
 
@@ -97,10 +121,14 @@ export default {
             phases: [],
             selectedPhase: this.movie.mcu_phase_id,
 
+            allDirectors: [],
             directors: [],
+            directorSearch: null,
             selectedDirectors: this.movie.directors,
 
+            allSagas: [],
             sagas: [],
+            sagaSearch: null,
             selectedSagas: this.movie.sagas,
 
             in_mcu: this.movie.in_mcu ? true : false,
@@ -152,6 +180,36 @@ export default {
             this.selectedDirectors.splice(director_index, 1);
         },
 
+
+        /**
+         * Filters directors according to the search term provided
+         */
+        searchDirectors: function() {
+
+            // If no directors
+            let results = [];
+            for (let i = 0; i < this.allDirectors.length; i++) {
+                const search = this.filter(this.allDirectors[i].name, this.directorSearch);
+                if (search == true) {
+                    results.push(this.allDirectors[i]);
+                }
+            }
+            this.directors = results;
+        },
+
+        /**
+         * Filters strings via a provided search term
+         *
+         * @param {String} text item to search against 
+         * @param {String} searchString term to search for
+         * @return {Bool}
+         */
+        filter: function(text, searchString) {
+            const regexStr = '(?=.*' + searchString.split(/\,|\s/).join(')(?=.*') + ')';
+            const searchRegEx = new RegExp(regexStr, 'gi');
+            return text.match(searchRegEx) !== null;
+        },
+
         /**
          * Adds specified saga to the selectedSagas array
          *
@@ -159,7 +217,6 @@ export default {
          */
         addSelectedSaga: function(saga) {
             this.selectedSagas.push(saga);
-            console.log(this.selectedSagas);
         },
 
         /**
@@ -173,11 +230,26 @@ export default {
         },
 
         /**
+         * Filters sagas according to the search term provided
+         */
+        searchSagas: function() {
+            let results = [];
+            for (let i = 0; i < this.allSagas.length; i++) {
+                const search = this.filter(this.allSagas[i].title, this.sagaSearch);
+                if (search == true) {
+                    results.push(this.allSagas[i]);
+                }
+            }
+            this.sagas = results;
+        },
+
+        /**
          * Fetch all directors
          */
         fetchDirectors: function() {
             axios.get('/api/directors')
                 .then(res => {
+                    this.allDirectors = res.data.data;
                     this.directors = res.data.data;
                     const movieDirectors = [];
 
@@ -210,11 +282,15 @@ export default {
         fetchSagas: function() {
             axios.get('/api/sagas')
                 .then(res => {
-                    this.sagas = res.data.data
+                    this.allSagas = res.data.data;
+                    this.sagas = res.data.data;
                 })
                 .catch(e => console.log(e))
         },
 
+        /**
+         * Updates movie instance
+         */
         updateMovie: function() {
             this.movie.directors = this.selectedDirectors;
             this.movie.mcu_phase_id = this.selectedPhase;
@@ -223,7 +299,6 @@ export default {
             axios.patch(`/api/movies/${this.movie.id}`, this.movie )
                 .then(res => {
                     this.$emit('closeEditModal');
-                    console.log(this.movie.directors);
                 })
                 .catch(e => {
                     if (e.response && e.response.status == 422) { this.validationErrors = e.response.data.errors }
@@ -236,6 +311,21 @@ export default {
         movie: {
             type: [Array, Object],
             required: true
+        }
+    },
+    watch: {
+        /**
+         * Watches for changes on directorSearch variable; runs function if changed
+         */
+        directorSearch: function() {
+            this.searchDirectors();
+        },
+
+        /**
+         * Watches for changes on sagaSearch variable; runs function if changed
+         */
+        sagaSearch: function() {
+            this.searchSagas();
         }
     }
 }
