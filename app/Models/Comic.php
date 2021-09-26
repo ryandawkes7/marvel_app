1a05a0e9e9195bc9bb6b97f7853ff2940afc74bd
Ryan Dawkes <rd@moose.co>
2021-09-26 14:17:21 +0100

    Add new rels and pull out characters and covers in with

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,14 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comic extends Model
 {
     use HasFactory;
 
+    protected $with = ['characters', 'covers'];
+
     public function covers() :HasMany
     {
         return $this->hasMany(ComicCover::class, 'comic_id', 'id');
+    }
+
+    public function comicBook() :BelongsTo
+    {
+        return $this->belongsTo(ComicBook::class, 'comic_book_id', 'id');
+    }
+
+    public function characters() :BelongsToMany
+    {
+        return $this->belongsToMany(Character::class, CharacterComic::class, 'comic_id', 'character_id', 'id', 'id');
     }
 }
