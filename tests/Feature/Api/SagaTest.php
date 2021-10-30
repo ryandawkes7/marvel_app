220676a0a97df540bf63c52ee0773f10f0b404ee
Ryan Dawkes <rd@moose.co>
2021-10-30 10:55:59 +0100

    Testing /sagas endpoint

---
+++ tests/Feature/Api/SagaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\ApiTest;
+use Tests\TestCase;
+
+class SagaTest extends ApiTest
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_fetching_all_sagas()
+    {
+        $response = $this->getJson("{$this->path_prefix}/sagas");
+        $response->assertOk();
+        $response->assertJsonStructure(['data', 'message']);
+    }
+
+    public function test_fetching_all_sagas_when_there_are_no_saga_records_in_db()
+    {
+        $this->flushSagas();
+        $response = $this->getJson("{$this->path_prefix}/sagas");
+        
+        $response->assertNotFound();
+        $response->assertJson([
+            'data' => null,
+            'message' => "No sagas could be found"
+        ]);
+        $this->assertDatabaseCount('movie_sagas', 0);
+    }
+}
