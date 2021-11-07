88789e4c07f8407012c07f12b46a841e89c3307a
Ryan Dawkes <rd@moose.co>
2021-11-07 13:23:11 +0000

    Setup factory, fillables and no tiemstamps for character types

--- app/Models/CharacterType.php
+++ app/Models/CharacterType.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use Database\Factories\Test\CharacterTypeFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CharacterType extends Model
 {
     use HasFactory;
+    protected static function newFactory()
+    {
+        return CharacterTypeFactory::new();
+    }
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'key',
+        'type',
+    ];
 
     public function characters()
     {
