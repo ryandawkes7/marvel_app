a3ba418b7bfa09c83e591bb02526fc68702bf3a6
Ryan Dawkes <rd@moose.co>
2021-11-07 14:53:23 +0000

    Create Requests for storing and updating traits

---
+++ app/Http/Requests/StoreTraitRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+class StoreTraitRequest extends ApiFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'          => 'required|string|unique:traits,name',
+            'description'   => 'required|string'
+        ];
+    }
+}
