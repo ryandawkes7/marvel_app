35f8970f113f46994249cb7da70f4ac7f951c80e
Ryan Dawkes <rd@moose.co>
2021-11-08 16:35:38 +0000

    Create factory and test seeder for director

--- database/seeders/Test/DirectorSeeder.php
+++ database/seeders/Test/DirectorSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders\Test;
 
-use Carbon\Carbon;
+use App\Models\Director;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DirectorSeeder extends Seeder
 {
@@ -15,12 +14,6 @@ class DirectorSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('directors')->insert([
-            [
-                'name'  => 'Testing Director',
-                'dob'   => Carbon::parse('1966-08-05'),
-                'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/James_Gunn_%2828557194032%29_%28cropped%29.jpg/1024px-James_Gunn_%2828557194032%29_%28cropped%29.jpg'
-            ]
-        ]);
+        Director::factory()->count(3)->create();
     }
 }
