ce1fcaefac9df1961ea03eb0c0338bf72bc54a13
Ryan Dawkes <rd@moose.co>
2021-11-14 18:06:31 +0000

    Wrap routes behind auth wall

--- routes/api.php
+++ routes/api.php
@@ -16,41 +16,53 @@ use App\Http\Controllers\TraitsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Fallback route
+Route::fallback(function () {
+    return response()->json([
+        "message" => "Invalid Request"
+    ], 500);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('characters', CharactersController::class);
-Route::apiResource('character-types', CharacterTypesController::class);
-
-Route::apiResource('traits', TraitsController::class);
-
-// TODO: Is this necessary?
-Route::apiResource('character-traits', CharacterTraitsPivotController::class);
-
-Route::apiResource('skills', SkillsController::class);
-
-Route::apiResource('movies', MoviesController::class);
-Route::apiResource('directors', DirectorsController::class);
-Route::apiResource('posters', MoviePostersController::class);
-
-Route::apiResource('phases', McuPhaseController::class);
-Route::apiResource('sagas', SagasController::class);
-
-Route::prefix('comics')->group(function () {
-    Route::get('/', [ComicBooksController::class, 'index']);
-    Route::get('/{id}', [ComicBooksController::class, 'show']);
-    Route::post('/', [ComicBooksController::class, 'store']);
-    Route::put('/{id}', [ComicBooksController::class, 'update']);
-    Route::delete('/{id}', [ComicBooksController::class, 'destroy']);
-
-    Route::prefix('/{comic_id}/issues')->group(function() {
-        Route::get('/', [ComicsController::class, 'index']);
-        Route::get('/{issue_id}', [ComicsController::class, 'show']);
-        Route::post('/', [ComicsController::class, 'store']);
-        Route::put('/{issue_id}', [ComicsController::class, 'update']);
-        Route::delete('/{issue_id}', [ComicsController::class, 'destroy']);
-    });
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+    return ['token' => $token->plainTextToken];
 });
 
-Route::apiResource('comic-writers', ComicWritersController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('characters', CharactersController::class);
+    Route::apiResource('character-types', CharacterTypesController::class);
+    Route::apiResource('traits', TraitsController::class);
+    Route::apiResource('character-traits', CharacterTraitsPivotController::class); //TODO: Is this endpoint necessary?
+
+
+    Route::apiResource('skills', SkillsController::class);
+
+    Route::apiResource('movies', MoviesController::class);
+    Route::apiResource('directors', DirectorsController::class);
+    Route::apiResource('posters', MoviePostersController::class);
+
+    Route::apiResource('phases', McuPhaseController::class);
+    Route::apiResource('sagas', SagasController::class);
+
+    Route::prefix('comics')->group(function () {
+        Route::get('/', [ComicBooksController::class, 'index']);
+        Route::get('/{id}', [ComicBooksController::class, 'show']);
+        Route::post('/', [ComicBooksController::class, 'store']);
+        Route::put('/{id}', [ComicBooksController::class, 'update']);
+        Route::delete('/{id}', [ComicBooksController::class, 'destroy']);
+
+        Route::prefix('/{comic_id}/issues')->group(function() {
+            Route::get('/', [ComicsController::class, 'index']);
+            Route::get('/{issue_id}', [ComicsController::class, 'show']);
+            Route::post('/', [ComicsController::class, 'store']);
+            Route::put('/{issue_id}', [ComicsController::class, 'update']);
+            Route::delete('/{issue_id}', [ComicsController::class, 'destroy']);
+        });
+    });
+
+    Route::apiResource('comic-writers', ComicWritersController::class);
+});
