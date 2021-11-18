4582d6179e99f6ee737c47889ed5a7905daf7db9
Ryan Dawkes <rd@moose.co>
2021-11-18 16:51:59 +0000

    UI suport for actors

---
+++ resources/js/Pages/Actors/ContentGrid.vue
@@ -0,0 +1,72 @@
+<template>
+    <!-- Character List -->
+    <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <!-- Boxes -->
+        <li
+            v-for="actor in actors"
+            :key="actor.id"
+            @click="show(actor.id)"
+            class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200 border transition-all cursor-pointer hover:border-indigo-500 hover:shadow-xl"
+        >
+            <div class="flex-1 flex flex-col p-8">
+                <!-- Image -->
+                <div class="w-32 h-32 flex-shrink-0 mx-auto bg-white rounded-full overflow-hidden shadow-lg">
+                    <img class="object-contain min-w-full min-h-full"
+                        :src="actor.image_url
+                            ? actor.image_url
+                            : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ZsrCV912yo&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'"
+                    />
+                </div>
+
+                <!-- Alias -->
+                <h3 class="mt-6 text-gray-900 text-md font-semibold">
+                    {{ actor.name }}
+                </h3>
+
+                <!-- Real Name -->
+                <p class="text-gray-500 text-xs font-light">
+                    {{ actor.dob }}
+                </p>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            actors: this.actors
+        }
+    },
+    props: [
+        'actors'
+    ],
+    methods: {
+        /**
+         * Fetches all actors
+         *
+         * @return void
+         */
+        fetchActors: function() {
+            axios.get('/api/actors')
+                .then(res => {
+                    this.actors = res.data.data;
+                })
+                .catch(e => console.log(e));
+        }
+        /**
+         * Redirects to show page
+         *
+         * @param integer character_id 
+         * @return object
+         */
+        show(id) {
+            return window.location.href = `/actors/${id}`;
+        },
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/Pages/Actors/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <app-layout>
+    <!-- Header -->
+    <div class="flex flex-col gap-8">
+        <section class="flex justify-between items-center">
+            <!-- Title -->
+            <div class="font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                <h1>Characters</h1>
+            </div>
+
+            <!-- Create Character -->
+            <div>
+                <button type="button" @click="isCreateModalOpen = true" class="px-3 py-1 text-green-800 flex items-center gap-2 border border-green-500 transition-colors rounded-md bg-green-300 hover:bg-green-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" /></svg>
+                    <span>Create Actor</span>
+                </button>
+            </div>
+        </section>
+
+        <ContentGrid></ContentGrid>
+
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout';
+import { ref } from 'vue';
+import { ContentGrid } from './ContentGrid.vue';
+
+export default {
+    setup() {
+        const isCreateModalOpen = ref(false);
+
+        return {
+            isCreateModalOpen
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+        AppLayout,
+        ContentGrid
+    }
+}
+</script>
+
+<style>
+
+</style>
